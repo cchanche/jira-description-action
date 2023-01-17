0986362e8e950c2ca5b2e7f2d927f3e8983104e4
Clément Chanchevrier <clement.chanchevrier@gmail.com>
2023-01-17 18:59:46 +0100

    feat: 🚧 log error stack

--- src/main.ts
+++ src/main.ts
@@ -27,7 +27,7 @@ async function run(): Promise<void> {
     await githubConnector.updatePrDetails(details);
   } catch (error) {
     console.log('JIRA key was not found');
-    console.error(error);
+    console.log(error);
     core.error(error);
 
     if (FAIL_WHEN_JIRA_ISSUE_NOT_FOUND) {
